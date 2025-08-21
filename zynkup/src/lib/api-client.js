ae9d51a3219ddca3dfbd147f80f68557933ee9e0
Pooja675 <poojasinha675@gmail.com>
2025-08-21 11:10:50 -0400

    updated

--- zynkup/src/lib/api-client.js
+++ zynkup/src/lib/api-client.js
@@ -4,7 +4,7 @@ import axios from "axios"
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? import.meta.env.VITE_API_URL || "http://localhost:5555"
-    : "/api";
+    : "";
 
 export const apiClient = axios.create({
     // baseURL: HOST

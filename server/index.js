8cde3da8ea89c14b2b2187052a9b3997c5ed3040
Pooja675 <poojasinha675@gmail.com>
2025-08-21 09:42:16 -0400

    Updated

--- server/index.js
+++ server/index.js
@@ -8,20 +8,35 @@ import contactsRoutes from "./routes/ContactRoutes.js";
 import setupSocket from "./socket.js";
 import messagesRoutes from "./routes/MessagesRoutes.js";
 import channelRoutes from "./routes/ChannelRoutes.js";
+import path from "path"
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 const databaseURL = process.env.DATABASE_URL;
+const _dirname = path.resolve()
+
+
+// app.use(
+//     cors({
+//         origin: [process.env.ORIGIN],
+//         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//         credentials: true,
+//     })
+// )
 
 app.use(
-    cors({
-        origin: [process.env.ORIGIN],
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-        credentials: true,
-    })
-)
+  cors({
+    origin:
+      process.env.ORIGIN === "*"
+        ? true
+        : (process.env.ORIGIN || "http://localhost:5173").trim(),
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
 
 app.use("/uploads/profiles", express.static("uploads/profiles"))
 app.use("/uploads/files", express.static("uploads/files"))
@@ -33,6 +48,15 @@ app.use("/api/auth", authRoutes)
 app.use("/api/contacts", contactsRoutes)
 app.use("/api/messages", messagesRoutes)
 app.use("/api/channel", channelRoutes)
+
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(_dirname, "../zynkup/dist")))
+}
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.resolve(_dirname, "../zynkup/dist/index.html"));
+});
+
 
 const server = app.listen(port, () => {
   console.log(`Server is running at PORT number: ${port}`);
--- zynkup/src/lib/api-client.js
+++ zynkup/src/lib/api-client.js
@@ -1,6 +1,12 @@
 import {HOST} from "../utils/constant"
 import axios from "axios"
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_API_URL || "http://localhost:5555"
+    : "/api";
+
 export const apiClient = axios.create({
-    baseURL: HOST
+    // baseURL: HOST
+    baseURL: BASE_URL
 })

77cea70cd793b961bf984d4828e60478f3fb142f
Pooja675 <poojasinha675@gmail.com>
2025-08-22 18:17:33 -0400

    updated

--- zynkup/src/pages/auth/index.jsx
+++ zynkup/src/pages/auth/index.jsx
@@ -63,7 +63,7 @@ const Auth = () => {
         { withCredentials: true }
       );
       if (response.data.user.id) {
-         setUserInfo(response.data?.user)
+         setUserInfo(response?.data?.user)
         if (response.data.user?.profileSetup) navigate("/chat");
         else navigate("/profile");
       }

274d51e4bcea22ee11c668ad1c065525a7d0c1aa
Pooja675 <poojasinha675@gmail.com>
2025-11-14 16:38:27 -0500

    updated changes

--- zynkup/src/utils/constant.js
+++ zynkup/src/utils/constant.js
@@ -3,7 +3,7 @@
 export const HOST =
   import.meta.env.MODE === "development"
     ? import.meta.env.VITE_SERVER_URL || "http://localhost:5555"
-    : import.meta.env.VITE_RENDER_BACKEND_URL;
+    : window.location.origin;
 
 export const AUTH_ROUTES = "/auth";
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
